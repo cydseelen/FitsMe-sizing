b341c05033367fc89da65ed1e5d44f31df283f64
amber lee <amblynlee@gmail.com>
2021-02-27 17:07:55 +0100

    updated seeds and path to move

--- app/models/product_size.rb
+++ app/models/product_size.rb
@@ -1,22 +1,25 @@
 class ProductSize < ApplicationRecord
+  SIZES = ["extra small", "small", "medium", "large", "extra large"]
+
+
   belongs_to :product
   validates :size_name, inclusion: {in: ["extra small", "small", "medium", "large", "extra large"] }
 
   before_save :convert_product_size_data
 
   def convert_product_size_data
-    all_sizes = self.product.product_sizes
-    extra_small_product_size = get_product_size("extra small", all_sizes)
-    small_product_size = get_product_size("small", all_sizes)
-    medium_product_size = get_product_size("medium", all_sizes)
-    large_product_size = get_product_size("large", all_sizes)
-    extra_large_product_size = get_product_size("extra large", all_sizes)
+    # all_sizes = self.product.product_sizes
+    # extra_small_product_size = get_product_size("extra small", all_sizes)
+    # small_product_size = get_product_size("small", all_sizes)
+    # medium_product_size = get_product_size("medium", all_sizes)
+    # large_product_size = get_product_size("large", all_sizes)
+    # extra_large_product_size = get_product_size("extra large", all_sizes)
 
 
-    self.hip_range = (@hip_measurement.extra_small_product_size - 10) -@hipmeasuresmallersize   * 2
-    self.bust_range =
-    self.waist_range =
-    self.length_range =
+    # self.hip_range = (@hip_measurement.extra_small_product_size - 10) -@hipmeasuresmallersize   * 2
+    # self.bust_range = 1
+    # self.waist_range = 1
+    # self.length_range = 1
   end
 
 private
