958eea2057be63d053262273d365fbc0f744bb12
Svenja Tegtmeier <tegtsven@isu.edu>
2021-03-06 11:25:22 +0100

    design fit added instead of fabric type, xs xl calculations changed

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ApplicationRecord
   belongs_to :brand
   has_many :product_sizes
   validates :garment_type, inclusion: {in:  ["shirt", "pants", "jacket", "skirt", "dress", "sweater"] }
-  validates :fabric_type, inclusion: {in: ["polyester", "cotton", "wool", "jersey", "modal", "silk", "spandex", "tweed", "twill", "viscose", "leather", "lace", "linen", "other natural", "other synthetic"] }
+  validates :fabric_type, inclusion: {in: ["tight less than 5cm allowance", "medium +5 to 10cm allowance", "loose +10cm allowance"] }
 end
