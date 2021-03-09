74ac6b3ac60f6c0c37080c0101b5c92bba2dde26
Cydnie Seelenbinder <cydalexand@gmail.com>
2021-03-09 21:20:02 +0100

    Added JS to Quiz submit button & changed size rec back to capital letters instead of full words.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def display_abbreviation(word)
+    if word == "extra small"
+      "XS"
+    elsif word == "small"
+      "S"
+    elsif word == "medium"
+      "M"
+    elsif word == "large"
+      "L"
+    elsif word == "extra large"
+      "XL"
+    end
+  end
 end
